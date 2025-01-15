d1fca3bdf791ec691781b5694555742f4a25b74a
Torin Dailey <torindailey1@gmail.com>
2025-01-15 16:54:15 -0500

    small changes

--- api_script.py
+++ api_script.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 #File location for a read_me file
 @app.route('/read_me')
 def read_me():
-        return 'Some placeholder text'
+        return "Some placeholder text"
 
 #Placeholder for the location of data
-@app.route('/data/')
+@app.route('/data')
 def data_output():
-        return 'example data placeholder'
+        return "example data placeholder"
+
+#start the app
+if __name__ == "__main__":
+       app.run(debug=True)
